25814f1d3e15bde50e61109828fdb38d52e1deea
MirandaLv <zhonghui.lv@gmail.com>
2023-06-15 19:39:35 +0800

    add processed data

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -5,6 +5,7 @@ from os.path import dirname as up
 
 import rasterio
 import pandas as pd
+import geopandas as gpd
 import numpy as np
 
 from rasterio.plot import show
@@ -17,7 +18,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 root_dir = up(os.getcwd())
-vector_dir = os.path.join(root_dir, 'data/vectors')
+
+# set file paths
+label_path = os.path.join(root_dir, 'processing_data/vectors/marsh_community.shp')
+image_path = os.path.join(root_dir, 'processing_data/images/2018-07-01.tif')
+
+# read in label data
+labels_gdf = gpd.read_file(label_path)
+# read in image data
+image_src = rasterio.open(image_path)
+
+# check on the crs of the shapefile
+
+
+# check on the crs of the images
+
+
+
+
+
 
 
 

36d74cfc123d2f0d783010229401b1e0fe4c4bdd
MirandaLv <zhonghui.lv@gmail.com>
2023-07-21 12:10:41 -0400

    add random forest code for testing, parameters are default, need to be tuned

--- scripts/data_analysis_all.py
+++ scripts/data_analysis_all.py
@@ -1,16 +1,19 @@
 
 import os
 from os.path import dirname as up
+import time
 
 # data processing
 import geopandas as gpd
 import numpy as np
+import pandas as pd
 
 # machine learning
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 import lightgbm as lgb
 import xgboost as xgb
 from sklearn import tree
@@ -55,6 +58,7 @@ X_scaled = scaler.transform(X_data)
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y_data, test_size=0.20, random_state=42, shuffle=True) # , stratify = y_data.ravel()
 
 
+print(X_train.shape)
 
 # parameter searching
 """
@@ -87,30 +91,30 @@ print(f"Accuracy with SVM: {accuracy_score(y_test, svm_pred)*100}")
 print(classification_report(y_test, svm_pred))
 
 
-
-"""
-LightGBM
--> Figure out parameters
-"""
-d_train = lgb.Dataset(X_train, label=y_train)
-# Parameters
-params={}
-params['learning_rate']=0.03
-params['boosting_type']='gbdt' #GradientBoostingDecisionTree
-params['objective']='multiclass' #Multi-class target feature
-params['metric']='multi_logloss' #metric for multi-class
-params['max_depth']=15
-params['num_class']=6 #no.of unique values in the target class not inclusive of the end value
-
-clf = lgb.train(params, d_train, 100)
-
-# prediction
-lgb_predictions = clf.predict(X_test)
-lgb_pred = np.argmax(lgb_predictions, axis=1)
-
-# Accuracy and Classification Report
-print(f"Accuracy: {accuracy_score(y_test, lgb_pred)*100}")
-print(classification_report(y_test, lgb_pred))
+#
+# """
+# LightGBM
+# -> Figure out parameters
+# """
+# d_train = lgb.Dataset(X_train, label=y_train)
+# # Parameters
+# params={}
+# params['learning_rate']=0.03
+# params['boosting_type']='gbdt' #GradientBoostingDecisionTree
+# params['objective']='multiclass' #Multi-class target feature
+# params['metric']='multi_logloss' #metric for multi-class
+# params['max_depth']=15
+# params['num_class']=6 #no.of unique values in the target class not inclusive of the end value
+#
+# clf = lgb.train(params, d_train, 100)
+#
+# # prediction
+# lgb_predictions = clf.predict(X_test)
+# lgb_pred = np.argmax(lgb_predictions, axis=1)
+#
+# # Accuracy and Classification Report
+# # print(f"Accuracy: {accuracy_score(y_test, lgb_pred)*100}")
+# # print(classification_report(y_test, lgb_pred))
 
 
 """
@@ -120,8 +124,8 @@ tree_clf = tree.DecisionTreeClassifier()
 tree_clf = tree_clf.fit(X_train, y_train)
 tree_pred = tree_clf.predict(X_test)
 
-print(f"Accuracy with Decision Tree: {accuracy_score(y_test, tree_pred)*100}")
-print(classification_report(y_test, tree_pred))
+# print(f"Accuracy with Decision Tree: {accuracy_score(y_test, tree_pred)*100}")
+# print(classification_report(y_test, tree_pred))
 
 tree.plot_tree(tree_clf)
 dot_data = tree.export_graphviz(tree_clf, out_file=None)
@@ -131,11 +135,28 @@ graph.render("../figures/DecisionTree")
 
 
 """
-Random forest
+Random forest (parameters has not been tuned, code here for meeting display)
 """
+feature_names = X_data.columns.tolist()
+forest = RandomForestClassifier(random_state=0)
+rf_clf = forest.fit(X_train, y_train)
 
+start_time = time.time()
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
 
+# getting variable importance
+forest_importances = pd.Series(importances, index=feature_names)
 
+fig, ax = plt.subplots()
+forest_importances.plot.bar(yerr=std, ax=ax)
+ax.set_title("Feature importances using MDI")
+ax.set_ylabel("Mean decrease in impurity")
+plt.savefig('../figures/rf_VariableImportance.png')
+
+rf_pred = rf_clf.predict(X_test)
+print(f"Accuracy with Random Forest: {accuracy_score(y_test, rf_pred)*100}")
+print(classification_report(y_test, rf_pred))
 
 
 """
